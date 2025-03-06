78ea30ebb9de9b1a109a642803ae04633c0158aa
iamriatandan <tandanria@gmail.com>
2025-03-06 23:38:30 +0530

    Ria Added . UC7 Noting logging levels using Slfj4

--- src/main/java/com/company/addressbookapp/demo/dto/AddressBookDTO.java
+++ src/main/java/com/company/addressbookapp/demo/dto/AddressBookDTO.java
@@ -1,38 +1,12 @@
 package com.company.addressbookapp.demo.dto;
 
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
+    private Long index;
     private String name;
     private String email;
     private String phoneNumber;
-
-    public AddressBookDTO(){}
-    public AddressBookDTO(String name , String email, String phoneNumber){
-        this.name=name;
-        this.email=email;
-        this.phoneNumber=phoneNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
